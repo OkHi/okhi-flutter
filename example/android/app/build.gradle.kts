76e03730ed6c5afbdcc7aa0ca6270b2e9cedff3f
GransonO <oyombegranson@gmail.com>
2026-02-26 16:27:05 +0300

    CICD updates

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     namespace = "io.okhi.flutter.okhi_flutter_example"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    flutterNdkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -29,7 +29,7 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         multiDexEnabled = true
-        ndkVersion = "29.0.13599879" // Added this for my local environment.
+        ndkVersion = flutterNdkVersion //"29.0.13599879" // Added this for my local environment.
     }
 
     buildTypes {
